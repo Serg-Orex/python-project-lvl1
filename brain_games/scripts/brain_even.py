4f4c5f31d669ad7d673e2e42dda60ff5151bb2b9
s_orex <so7577@yandex.ru>
2022-07-17 13:12:32 +0300

    modified files and add brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,37 @@
+import random
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = input('Mai I have your name? ')
+    print(f'''Hello, {name}!
+Answer "yes" if the number is even, otherwise answer "no".''')
+
+    i = 0
+    while i < 3:
+        num = random.randint(-1000, 1500)
+        print(f'Question: {num}')
+        answ = input('Your answer: ')
+        if num % 2 == 0:
+            if answ == 'yes':
+                print('Correct!')
+                i += 1
+            else:
+                print(''''no' is wrong answer ;(. Correct answer was 'yes'.
+Let's try again, {name}!''')
+                exit()
+        else:
+            if answ == 'no':
+                print('Correct!')
+                i += 1
+            else:
+                print(f''''yes' is wrong answer ;(. Correct answer was 'no'.
+Let's try again, {name}!''')
+                exit()
+    print(f'Congratulations, {name}!')
+
+if __name__ == '__main__':
+    main()
+
+
+
