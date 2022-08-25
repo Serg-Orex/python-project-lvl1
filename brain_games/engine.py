79ef412d2ca586c693133a1bde0baa69ae95427e
s_orex <so7577@yandex.ru>
2022-08-25 09:45:36 +0300

    modified enjine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,17 +4,16 @@ import prompt
 def get_game_engine(game):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
-    print(f'''Hello, {name}!
-{game.DISCRIPSION}''')
-    number_of_game_rounds = 3
-    for i in range(number_of_game_rounds):
+    print(f'Hello, {name}!')
+    print(f'{game.DISCRIPSION}')
+    game_rounds_count = 3
+    for i in range(game_rounds_count):
         question_game, correct_answer = game.get_question_and_answer()
         print(f'Question: {question_game}')
         user_answer = input('Your answer: ')
-        if user_answer == correct_answer:
-            print('Correct!')
-        else:
+        if user_answer != correct_answer:
             return print(f"'{user_answer}' is wrong answer ;(."
                          f" Correct answer was '{correct_answer}'.\n"
                          f"Let's try again, {name}!")
-    return print(f'Congratulations, {name}!')
+            return print('Correct!')
+    print(f'Congratulations, {name}!')
