f5611160c53178f0f19483dada578e10e2262121
s_orex <so7577@yandex.ru>
2022-08-25 09:28:41 +0300

    modified files

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,8 +6,5 @@ DISCRIPSION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def get_question_and_answer():
     question_game = random.randint(-1000, 1500)
-    if question_game % 2 == 0:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
+    correct_answer = 'yes' if question_game % 2 == 0 else 'no'
     return question_game, str(correct_answer)
